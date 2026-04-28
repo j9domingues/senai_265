a928a6299d6b3d22d8fdb6fe089e57c23d7f1d8c
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2026-04-27 21:45:29 -0300

    aula 1

--- lacos.py
+++ lacos.py
@@ -1,1 +1,13 @@
-print("olá git")
+
+## Aula  if elif else 
+
+sinal = input("Digite o sinal:")
+
+if sinal == "vermelho": 
+    print("Pare!")
+elif sinal == "amarelo":
+    print("Atenção!")
+elif sinal == "verde":
+     print("Prossiga!")
+else:
+    print("Sinal errado!")
